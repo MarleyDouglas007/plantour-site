649b7952bd5b0fd21d8111e15d8293cea2ca22f2
MarleyDouglas007 <marley.santos2308@gmail.com>
2024-07-26 16:19:09 -0300

    arrumando a página / add novo carrosel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import './App.css';
 import { Carousel } from 'react-responsive-carousel';
@@ -7,6 +6,16 @@ import imagem1 from './imagens/imagem1.jpg';
 import imagem2 from './imagens/imagem2.jpg';
 import imagem3 from './imagens/imagem3.jpg';
 import logo from './imagens/LogoPlantour.png';
+import splash from './imagens/splash.jpeg';
+import login from './imagens/login.jpeg';
+import perfil1 from './imagens/perfil1.jpg';
+import perfil2 from './imagens/perfil2.jpeg';
+import perfil3 from './imagens/perfil3.jpeg';
+import perfil4 from './imagens/perfil4.jpg';
+import perfil5 from './imagens/perfil5.jpg';
+import postaCard from './imagens/postaCard.jpeg';
+import roteiro from './imagens/roteiro.jpeg';
+import configuracao from './imagens/configuracao.jpeg';
 
 function App() {
   return (
@@ -15,7 +24,6 @@ function App() {
         <div className="container">
           <div className="logo-container">
             <img src={logo} alt="Recursos do Plantour" className="logo" />
-            <h1 className="logo-text">Plantour</h1>
           </div>
           <nav>
             <ul className="nav-links">
@@ -27,10 +35,61 @@ function App() {
         </div>
       </header>
       <main className="main-content">
-        <section id="about" className="about-section">
-          <div className="container">
-            <h2>Sobre o Plantour</h2>
-            <p>O Plantour é um aplicativo inovador que ajuda você a planejar suas viagens de forma eficiente e divertida. Com ele, você pode buscar destinos, encontrar pontos turísticos e organizar seus passeios em um só lugar.</p>
+        <section className="content-section">
+          <div id="about" className="about-section">
+            <div className="container about-container">
+              <div className="about-text">
+                <h2>Sobre o Plantour</h2>
+                <p>O Plantour é um aplicativo inovador que ajuda você a planejar suas viagens de forma eficiente e divertida. Com ele, você pode buscar destinos, encontrar pontos turísticos e organizar seus passeios em um só lugar.</p>
+              </div>
+              <Carousel
+                className="features-carousel"
+                showThumbs={false}
+                showStatus={false}
+                infiniteLoop
+                autoPlay
+                interval={3000}
+                transitionTime={500}
+              >
+                <div className="carousel-image-container">
+                  <img src={splash} alt="Sobre Plantour" className="carousel-image" />
+                </div>
+                <div className="carousel-image-container">
+                  <img src={login} alt="Sobre Plantour" className="carousel-image" />
+                </div>
+                <div className="carousel-image-container">
+                  <img src={imagem1} alt="Sobre Plantour" className="carousel-image" />
+                </div>
+                <div className="carousel-image-container">
+                  <img src={imagem2} alt="Recursos do Plantour" className="carousel-image" />
+                </div>
+                <div className="carousel-image-container">
+                  <img src={imagem3} alt="Logo Plantour" className="carousel-image" />
+                </div>
+              </Carousel>
+            </div>
+            <div className="comment-section">
+              <div className="comment">
+                <img src={perfil1} alt="Usuário 1" className="comment-image" />
+                <p>Excelente aplicativo! Planejei minhas férias perfeitamente com o Plantour! - João Silva</p>
+              </div>
+              <div className="comment">
+                <img src={perfil2} alt="Usuário 2" className="comment-image" />
+                <p>Muito prático e fácil de usar. Recomendo a todos os viajantes. - Mario Ferreira</p>
+              </div>
+              <div className="comment">
+                <img src={perfil5} alt="Usuário 4" className="comment-image" />
+                <p>O Plantour tornou o planejamento das minhas viagens muito mais divertido e fácil. - Juliana Costa</p>
+              </div>
+              <div className="comment">
+                <img src={perfil3} alt="Usuário 3" className="comment-image" />
+                <p>Adorei as recomendações de pontos turísticos!- Carlos Alberto</p>
+              </div>
+              <div className="comment">
+                <img src={perfil4} alt="Usuário 4" className="comment-image" />
+                <p>Organizei toda minha viagem com o Plantour. Sensacional! - Ana Paula</p>
+              </div>
+            </div>
           </div>
         </section>
         <section id="features" className="features-section">
@@ -46,26 +105,26 @@ function App() {
           </div>
         </section>
       </main>
-      <h2 >App Plantour</h2>
       <Carousel
-        className="features-carousel"
-        showThumbs={false}
-        showStatus={false}
-        infiniteLoop
-        autoPlay
-        interval={3000}
-        transitionTime={500}
-      >
-        <div className="carousel-image-container">
-          <img src={imagem1} alt="Sobre Plantour" className="carousel-image" />
-        </div>
-        <div className="carousel-image-container">
-          <img src={imagem2} alt="Recursos do Plantour" className="carousel-image" />
-        </div>
-        <div className="carousel-image-container">
-          <img src={imagem3} alt="Logo Plantour" className="carousel-image" />
-        </div>
-      </Carousel>
+                className="features-carousel"
+                showThumbs={false}
+                showStatus={false}
+                infiniteLoop
+                autoPlay
+                interval={3000}
+                transitionTime={500}
+              >
+                <div className="carousel-image-container">
+                  <img src={configuracao} alt="Sobre Plantour" className="carousel-image" />
+                </div>
+                <div className="carousel-image-container">
+                  <img src={roteiro} alt="Sobre Plantour" className="carousel-image" />
+                </div>
+                <div className="carousel-image-container">
+                  <img src={postaCard} alt="Sobre Plantour" className="carousel-image" />
+                </div>
+               
+              </Carousel>
       <footer className="footer">
         <div className="container">
           <p>&copy; 2024 Plantour. Todos os direitos reservados.</p>
