8c2dc95dd4dd481a2d471fb1d51a3cee454a858a
MarleyDouglas007 <marley.santos2308@gmail.com>
2024-07-26 01:43:22 -0300

    fazendo carrossel para melhorar a visualização do app/ arrumando pagina alguns componetes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// App.js
 import React from 'react';
 import './App.css';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import imagem1 from './imagens/imagem1.jpg';
+import imagem2 from './imagens/imagem2.jpg';
+import imagem3 from './imagens/imagem3.jpg';
+import logo from './imagens/LogoPlantour.png';
 
 function App() {
   return (
     <div className="App">
       <header className="header">
         <div className="container">
-          <h1 className="logo">Plantour</h1>
+          <div className="logo-container">
+            <img src={logo} alt="Recursos do Plantour" className="logo" />
+            <h1 className="logo-text">Plantour</h1>
+          </div>
           <nav>
             <ul className="nav-links">
               <li><a href="#about">Sobre</a></li>
@@ -24,16 +34,38 @@ function App() {
           </div>
         </section>
         <section id="features" className="features-section">
-          <div className="container">
-            <h2>Recursos</h2>
-            <ul className="features-list">
-              <li>Busca de destinos e pontos turísticos</li>
-              <li>Organização de roteiros de viagem</li>
-              <li>Informações detalhadas sobre locais</li>
-            </ul>
+          <div className="container features-carousel-container">
+            <div className="features-text">
+              <h2>Recursos</h2>
+              <ul className="features-list">
+                <li>Busca de destinos e pontos turísticos</li>
+                <li>Organização de roteiros de viagem</li>
+                <li>Informações detalhadas sobre locais</li>
+              </ul>
+            </div>
           </div>
         </section>
       </main>
+      <h2 >App Plantour</h2>
+      <Carousel
+        className="features-carousel"
+        showThumbs={false}
+        showStatus={false}
+        infiniteLoop
+        autoPlay
+        interval={3000}
+        transitionTime={500}
+      >
+        <div className="carousel-image-container">
+          <img src={imagem1} alt="Sobre Plantour" className="carousel-image" />
+        </div>
+        <div className="carousel-image-container">
+          <img src={imagem2} alt="Recursos do Plantour" className="carousel-image" />
+        </div>
+        <div className="carousel-image-container">
+          <img src={imagem3} alt="Logo Plantour" className="carousel-image" />
+        </div>
+      </Carousel>
       <footer className="footer">
         <div className="container">
           <p>&copy; 2024 Plantour. Todos os direitos reservados.</p>
